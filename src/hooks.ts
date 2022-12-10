5d2592adadf9407a00680ee1ddb81e3f8453771d
Anwesh43 <anweshthecool0@gmail.com>
2022-12-10 19:33:08 +0530

    adding useStlye hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, CSSProperties} from 'react'
 
 const delay : number = 20 
 const scGap : number = 0.01 
+const parts : number = 3
 
 export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
@@ -35,11 +36,41 @@ export const useDimension = () => {
             setH(window.innerHeight)
         }
         return () => {
-            
+
         }
     })
     return {
         w, 
         h
+    }
+}
+
+const maxScale : (a : number, b : number, c : number) => number = 
+    (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+
+const divideScale : (a : number, b : number, c : number) => number = 
+    (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const dsc : (scale : number) => number = (i : number) : number => divideScale(scale, i, parts)
+    const size : number = Math.min(w, h) / 10
+    return {
+        blockStyle() : CSSProperties {
+            const position = 'absolute'
+            const top = `${h / 2 - (h / 4) * dsc(0) + (h / 4) * dsc(1)}px`
+            const left = `${(w / 4) * (dsc(0) + dsc(1)) - (w / 2) * dsc(2)}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            const background = `indigo`
+            return {
+                position, 
+                left, 
+                top, 
+                width,
+                height,
+                background
+            }
+        },
     }
 }
