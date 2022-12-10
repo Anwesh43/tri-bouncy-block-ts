278c9591e1d7bb92470a5772f0b0947c34e3b3f3
Anwesh43 <anweshthecool0@gmail.com>
2022-12-10 19:16:29 +0530

    adding useAnimatedScale hook

---
+++ src/hooks.ts
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+
+const delay : number = 20 
+const scGap : number = 0.01 
+
+export const useAnimatedScale = () => {
+    const [scale, setScale] = useState(0)
+    const [animated, setAnimated] = useState(false)
+    return {
+        scale, 
+        start() {
+            if (!animated) {
+                setAnimated(true)
+                const interval = setInterval(() => {
+                    setScale((prev : number) : number => {
+                        if (prev > 1) {
+                            setAnimated(false)
+                            clearInterval(interval)
+                            return 0 
+                        }
+                        return prev + scGap 
+                    })
+                }, delay)
+            }
+        }
+    }
+}
